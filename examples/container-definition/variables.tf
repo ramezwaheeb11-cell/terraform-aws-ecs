2dd0518722e123779d7ca4a15c2a668225cfd8fb
VonDoo <ramezwaheeb11@gmail.com>
2026-08-27 01:02:12 +0300

    Delete examples directory

--- examples/container-definition/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "write_container_definition_to_file" {
-  description = "Determines whether the container definition JSON should be written to a file. Used for debugging and checking diffs"
-  type        = bool
-  default     = true
-}
